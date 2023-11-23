8e97f036a4afe0af11a6f23469b1193a5e6d8d38
wJumelle <wjumelle@gmail.com>
2023-11-23 19:07:07 +0100

    [TASK] Implémentation du router

--- league-of-legends/src/App.jsx
+++ league-of-legends/src/App.jsx
@@ -3,15 +3,34 @@ import './App.css';
 // Config
 import { VERSION } from './config';
 
+// Router
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
 // Composants
 import ChampionsList from './ChampionsList/ChampionsList';
 
 function App() {
   return (
-    <>
-      <ChampionsList v={VERSION} />
-    </>
+    <Router>
+      <nav>
+        <ul>
+          <li><Link to="/">Accueil</Link></li>
+          <li><Link to="/champions-list">Liste des champions</Link></li>
+          <li><Link to="/items-list">Liste des objets</Link></li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/" exact>
+          <ChampionsList v={VERSION} />
+        </Route>
+        <Route path="champions-list">
+          <ChampionsList v={VERSION} />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
+
+
 
 export default App
