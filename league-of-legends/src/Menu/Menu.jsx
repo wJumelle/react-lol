287eb9014f45d7d016d31706e31422a39ba99deb
wJumelle <wjumelle@gmail.com>
2023-11-25 19:43:37 +0100

    [TASK] Affichage du menu burger - FIX le fait qu'il ne soit pas centré

--- league-of-legends/src/Menu/Menu.jsx
+++ league-of-legends/src/Menu/Menu.jsx
@@ -9,16 +9,32 @@ import { Link } from 'react-router-dom';
 export default function Menu() {
     const [isOpenned, setIsOpenned] = useState(false);
 
+    function handleClickButtonMenu() {
+        setIsOpenned(state => state = !isOpenned);
+    }
+
+    const labelButtonMenu = (isOpenned) ? "Fermer le menu" : "Ouvrir le menu";
+    const classesButtonMenu = (isOpenned) ? "main-navigation__button is-openned" : "main-navigation__button is-closed";
+
     return (
         <>
+            <button onClick={handleClickButtonMenu} className={classesButtonMenu} aria-labe={labelButtonMenu}>
+                <span className="main-navigation__button-inner-container">
+                    <span className="line line1"></span>
+                    <span className="line line2"></span>
+                    <span className="line line3"></span>
+                </span>
+            </button>
             {isOpenned && 
-                <nav role="navigation" className="main-navigation">
-                    <ul>
-                        <li><Link to="/">Accueil</Link></li>
-                        <li><Link to="/champions-list">Liste des champions</Link></li>
-                        <li><Link to="/items-list">Liste des objets</Link></li>
-                    </ul>
-                </nav>
+                <div className="main-navigation__container">
+                    <nav role="navigation" className="main-navigation">
+                        <ul>
+                            <li><Link to="/">Accueil</Link></li>
+                            <li><Link to="/champions-list">Liste des champions</Link></li>
+                            <li><Link to="/items-list">Liste des objets</Link></li>
+                        </ul>
+                    </nav>
+                </div>
             }
         </>
         
